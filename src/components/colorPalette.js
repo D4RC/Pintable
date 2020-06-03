a6c0db5490fe2a4fa2402a68cbf6de735907201c
DARC <darc3012@gmail.com>
2020-06-02 21:33:54 -0500

    basic modules

---
+++ src/components/colorPalette.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ColorPalette(props) {
+
+}
+
+export default ColorPalette;
---
+++ src/components/mainLayout.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+//Subcompoentes
+import Board from './board';
+import ColorPalette from './colorPalette'
+
+function MainLayout() {
+
+}
+
+export default MainLayout;
