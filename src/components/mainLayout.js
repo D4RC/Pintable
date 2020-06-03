e07a6481ffee81ff5a407cde91eed2ea32021dd0
DARC <darc3012@gmail.com>
2020-06-02 21:40:02 -0500

    feat: Main component state

--- src/components/mainLayout.js
+++ src/components/mainLayout.js
@@ -4,8 +4,20 @@ import React, {useState, useEffect, useRef} from 'react';
 import Board from './board';
 import ColorPalette from './colorPalette'
 
-function MainLayout() {
+//Datos de la primera carga de la app
+const DAY_ZERO_MATRIX = Array(10).fill(Array(10).fill('#ffffff'));
 
+function MainLayout() {
+    //Hooks y estado
+    let [colorChosen, setColor] = useState('#000000');
+    let [colorGrid, setColorGrid] = useState(
+        window.localStorage.getItem('pintable') ? JSON.parse(localStorage.getItem('pintable')) : DAY_ZERO_MATRIX
+    );
+
+    const pintable = useRef();
+
+    //Efecto secundario: Almacenmaiento en local storage
+    useEffect(()=>window.localStorage.setItem('pintable',JSON.stringify(colorGrid)))
 }
 
 export default MainLayout;
